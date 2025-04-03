259040832146f4fb6d0ed34b2c136e427a4541c4
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2025-04-03 10:14:12 +0200

    Add color.h utility for colored terminal output
    Defines ANSI color codes and print macros for consistent and readable logging output.

---
+++ main/color.h
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+#ifndef COLOR_H
+#define COLOR_H
+
+static const char *black = "\033[0;30m";
+static const char *red = "\033[0;31m";
+static const char *green = "\033[0;32m";
+static const char *yellow = "\033[0;33m";
+static const char *blue = "\033[0;34m";
+static const char *magenta = "\033[0;35m";
+static const char *cyan = "\033[0;36m";
+static const char *white = "\033[0;37m";
+static const char *reset = "\033[0m";
+
+#define PRINTFC(color, format, ...) printf("%s" format "%s", color, ##__VA_ARGS__, reset)
+
+#endif
