b6290ab7ac7b7bd4803e09e00186f3ee4f73869c
Nazeh85 <160012102+Nazeh85@users.noreply.github.com>
2025-05-17 21:19:54 +0200

    Add header file for LCD1602 functions
    Created the header file `lcd1602.h` to define the functions for initializing, clearing, setting cursor position, and writing strings to an LCD1602 display via I2C. This file provides function prototypes for controlling the LCD display.

--- main/lcd1602.h
+++ main/lcd1602.h
@@ -8,4 +8,4 @@ void lcd1602_clear(uint8_t addr);
 void lcd1602_gotoxy(uint8_t addr, uint8_t col, uint8_t row);
 void lcd1602_write_string(uint8_t addr, const char *str);
 
-#endif // LCD1602_H
+#endif
